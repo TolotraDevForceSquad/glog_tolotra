bd7bc62dd6175be87ca66e6c328016acab276794
TolotraDevForceSquad <lucienravelo301@gmail.com>
2024-03-28 17:09:38 +0300

    Créez une classe Femme ayant les mêmes champs et des constructeurs

---
+++ src/iut/bat/Femme.java
@@ -0,0 +1,17 @@
+package iut.bat;
+
+public class Femme {
+    private String nom;
+    private String prenom;
+    private int age;
+
+    public Femme(String nom, String prenom, int age) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    
+    public Femme() {
+    	super();
+    }
+}
