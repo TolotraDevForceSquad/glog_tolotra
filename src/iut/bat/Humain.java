1f188cd3209c9b13f4ad4b59624dd5185e20b6d6
TolotraDevForceSquad <lucienravelo301@gmail.com>
2024-03-30 13:31:48 +0300

    Ajoutez une méthode ami(Humain) qui permet d'indiquer une amitié entre 2 humains

--- src/iut/bat/Humain.java
+++ src/iut/bat/Humain.java
@@ -1,14 +1,19 @@
 package iut.bat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Humain {
 	private String nom;
     private String prenom;
     private int age;
+    private List<Humain> amis;
     
     public Humain(String nom, String prenom, int age) {
     	this.nom = nom;
         this.prenom = prenom;
         this.age = age;
+        this.amis = new ArrayList<>();
     }
     
     public void details() {
@@ -25,5 +30,15 @@ public class Humain {
     
     public void boire() {
         System.out.println("Je bois.");
+    }
+    
+    public void ami(Humain ami) {
+        if (!this.amis.contains(ami)) {
+            this.amis.add(ami);
+            ami.ami(this);
+            System.out.println(this.prenom + " ami avec " + ami.prenom + ".");
+        } else {
+            System.out.println(this.prenom + " déjà ami avec " + ami.prenom + ".");
+        }
     }
 }
